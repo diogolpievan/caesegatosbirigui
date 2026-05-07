a5cd92a15ebd574760a5928d690cd69203c9c24d
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2026-05-07 11:25:52 -0300

    feature: navbar header

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,7 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
+import { NavItem } from "./nav-item";
 
-const Header = () => {
+const NAVIGATION_LINKS = [
+  {
+    label: 'Home', 
+    href: '/'
+  },
+  {
+    label: 'Sobre',
+    href: '/about'
+  },
+  {
+    label: 'Serviços',
+    href: '/services'
+  }
+]
+
+
+export const Header = () => {
   return (
     <header>
         <div className="container">
@@ -12,9 +29,14 @@ const Header = () => {
               alt="Logo Cães e Gatos Birigui"
             />
           </Link>
+
+          <nav>
+            {NAVIGATION_LINKS.map(({ label, href }) => (
+                <NavItem key={href} label={label} href={href} />
+            ))
+            }
+          </nav>
         </div>
     </header>
   )
 }
-
-export default Header;
