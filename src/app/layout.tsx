e77c99a80db0b734a17d1b24cc2158fe60f635dc
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2026-05-07 10:56:22 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Paytone_One, Montserrat } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/layout/header";
+
+const paytoneOne = Paytone_One({
+  variable: "--font-paytone-one",
+  subsets: ["latin"],
+  weight: "400"
+});
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"]
+});
+
+export const metadata: Metadata = {
+  title: "Cães e Gatos Birigui",
+  description: "Clínica especializada em dermatologia e odontologia veterinária para cães e gatos em Birigui. Cuidados odontológicos, tratamento de pele e cirurgias especializadas com equipe experiente.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="pt-BR"
+      className={`${paytoneOne.variable} ${montserrat.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/layout/header/index.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+        <div className="container">
+          <Link href="/">
+            <Image 
+              width={115}
+              src="/logo.png"
+              alt="Logo Caes e Gatos Birigui"
+            />
+          </Link>
+        </div>
+    </header>
+  )
+}
+
+export default Header;
